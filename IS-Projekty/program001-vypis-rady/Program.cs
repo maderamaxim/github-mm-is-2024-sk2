c543af1bcd6d7e9b4093aa81698a7bb255bba9c9
Maxim Maděra <maxim.madera@tul.cz>
2024-10-10 13:31:57 +0200

    #1 základní program s možostí opakování

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+
+    static void Main() {
+
+        // chci, aby se program opakoval po stisku klavesy "a"
+        string again = "a";
+        while(again =="a") {
+            Console.Clear();
+            Console.WriteLine("*************************");
+            Console.WriteLine("******* Výpis řady ******");
+            Console.WriteLine("*************************");
+            Console.WriteLine("****** Maxim Maděra *****");
+            Console.WriteLine("*************************\n\n");
+            Console.WriteLine();
+
+
+            // vstup od uživatele - TO DO
+
+            // Logika pro výpis řad - TO DO
+
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+
+    }
+
+    }
+}
