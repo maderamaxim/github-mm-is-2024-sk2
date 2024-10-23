a62ea77744550f586a1addde2bbca9767b856d5e
Maxim Maděra <maxim.madera@tul.cz>
2024-10-23 11:14:41 +0200

    #2 #3 domaci ukol

---
+++ IS-Projekty/program002b-soucet-cifer/Program.cs
@@ -0,0 +1,40 @@
+﻿ string again = "a";
+    while(again == "a") {
+    
+    Console.WriteLine("*************************************");
+    Console.WriteLine("******* Soucet a soucin cifer *******");
+    Console.WriteLine("*************************************");
+    Console.WriteLine("************ Maxim Maděra************");
+    Console.WriteLine("*************************************\n\n");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo: ");
+    int number;
+    while(!int.TryParse(Console.ReadLine(), out number)) {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu číslo: ");
+    }
+
+
+    // Výpis vstupní hodnoty
+    Console.WriteLine("=======================================");
+    Console.WriteLine("Uživatel zadal: {0}", number);
+    Console.WriteLine("=======================================\n\n");
+
+    int suma = 0;
+    int multi = 1;
+    int numberBackup = number;
+
+    string input = number.ToString();
+
+    foreach(char c in input){
+        int digit = c - '0';  
+        suma = suma + digit;
+        multi = multi * digit;
+}
+Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma);
+Console.WriteLine("\n\nSoučin cifer čísla {0} je {1}\n\n", numberBackup, multi);
+
+Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
---
+++ IS-Projekty/program003b-rovnoramenny-trojuhelnik/Program.cs
@@ -0,0 +1,25 @@
+﻿string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("******************************");
+            Console.WriteLine("******* Název programu *******");
+            Console.WriteLine("******************************");
+            Console.WriteLine("****** Maxim Maděra***********");
+            Console.WriteLine("**************************\n\n");
+            Console.WriteLine();
+
+            Console.Write("Zadejte délku přepony (celé číslo): ");
+            int rameno;
+            while(!int.TryParse(Console.ReadLine(), out rameno)){
+                Console.Write("Nezadali jste celé číslo, zadejte znovu délku přepony: ");
+            }
+            for(int x = 1; x <= rameno; x++){
+                for( int y = 1; y <= x; y++){
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }  
