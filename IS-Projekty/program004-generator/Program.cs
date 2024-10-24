8c050b70f8b262e45ffc1334b35f7a0cbacafe15
Maxim Maděra <maxim.madera@tul.cz>
2024-10-24 13:06:18 +0200

    #4 uzivatelsky vstup

--- IS-Projekty/program004-generator/Program.cs
+++ IS-Projekty/program004-generator/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("******* Generator pseudonahodnych cisel *******");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("**************** Maxim Maděra******************");
+            Console.WriteLine("***********************************************\n\n");
+            Console.WriteLine();
+
+
+            // Vstup od uživatele - lepší varianta
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
+
+             Console.Write("Zadejte dolní mez (celé číslo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
+
+             Console.Write("Zadejte horní mez (celé číslo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
+
+            Console.WriteLine("\n\n=============================================");
+            Console.WriteLine("Uživatelský vstup");
+            Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
+            Console.WriteLine("=============================================\n\n");
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
