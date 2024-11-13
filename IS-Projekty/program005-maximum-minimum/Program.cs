0ca06ae2dda714907d547087d4b9e80161a47866
maderamaxim <maxim.madera@tul.cz>
2024-11-13 20:27:11 +0100

    #5 domaci ukol

--- IS-Projekty/program005-maximum-minimum/Program.cs
+++ IS-Projekty/program005-maximum-minimum/Program.cs
@@ -45,22 +45,40 @@
                 myArray[i] = randomNumber.Next(dm, hm+1);
                 Console.Write("{0}; ", myArray[i]);
             }
-            
 
             
+            int countMin = 1;
+            int countMax = 1;
             int max = myArray[0];
             int min = myArray[0];
             int posMax = 0;
             int posMin = 0;
+            List<int> positionsMax = new List<int> {0};
+            List<int> positionsMin = new List<int> {0};
+        
 
             for(int i = 1; i < n; i++){
                 if(myArray[i] > max){
                     max = myArray[i];
+                    positionsMax.Clear();
+                    positionsMax.Add(i);
+                    countMax = 1;
                     posMax = i;
+                }
+                else if (myArray[i] == max){
+                    countMax++;
+                    positionsMax.Add(i);
                 }
                 if(myArray[i] < min){
                     min = myArray[i];
+                    positionsMin.Clear();
+                    positionsMin.Add(i)
+                    countMin = 1;
                     posMin = i;
+                }
+                else if (myArray[i] == min){
+                    countMin++;
+                    positionsMin.Add(i)
                 }
                 
             }
@@ -69,6 +87,18 @@
             Console.WriteLine("Minimum = {0}", min);
             Console.WriteLine("Pozice maxima je: {0}", posMax);
             Console.WriteLine("Pozice minima je: {0}", posMin);
+            Console.WriteLine("Počet výskytů maxima je: {0}", countMax);
+            Console.WriteLine("Počet výskytý minima je {0}", countMin);
+
+            Console.Write("Pozice všech maxim: ");
+            foreach (var pos in positionsMax) {
+                Console.Write("{0}", pos);
+            }
+            Console.WriteLine()
+            Console.Write("Pozice všech minim: ");
+            foreach (var pos in positionsMin) {
+                Console.Write("{0}, pos);
+            }
 
             // Opakování programu
             Console.WriteLine("\n\nPro opakování programu stiskněte klávesu a");
