e3438b85a3a6abbc87edce5230fbaf20bad5c32c
Maxim Maděra <maxim.madera@tul.cz>
2024-12-12 10:53:33 +0100

    #17 fibonacci

---
+++ IS-Projekty/program017b-fibonnaci-rekurzivne/Program.cs
@@ -0,0 +1,7 @@
+﻿static void Main(string[] args){
+    int vysledek = Fibonacci(5);
+    Console.WriteLine(vysledek);
+}
+static int Fibonacci(int n){
+    return n > 1 ? Fibonacci(n-1) + Fibonacci(n - 2) : n;
+}
